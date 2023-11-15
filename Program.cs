bc74bfec860bef29cd9c4f363d9f2031ccc848a8
S-Torkzadeh <147639939+S-Torkzadeh@users.noreply.github.com>
2023-11-15 19:23:27 +0330

    start

--- Program.cs
+++ Program.cs
@@ -1,47 +1,20 @@
-﻿// برنامه ای بنویسید که عددی خوانده و اگر فرد باشد مجموع و حاصل ضرب اعداد فرد تا ان عدد را حساب کند و اگر عدد خوانده شده زوج بود به همین صورت
+﻿// برنامه دریافت نمره میانترم و پایانترم یک دانشجو و نمایش نمره نهایی در متد اصلی
 using System;
-using System.ComponentModel.Design;
-using System.Diagnostics.CodeAnalysis;
-
 namespace Project
 {
-    class test
+    class tast
     {
         public static void Main()
         {
-            int x = Convert.ToInt32(Console.ReadLine());
-            if (x % 2 == 0)
-            {
-                Even(x);
-            }
-            else
-            {
-                Odd(x);
-            }
+            double m = Convert.ToDouble(Console.ReadLine());
+            double p = Convert.ToDouble(Console.ReadLine());
+            double res = Calc(m, p);
+            Console.WriteLine(res);
         }
-        static void Even(int x)
+        static double Calc(double m, double p)
         {
-            int sum = 0;
-            int mul = 1;
-            for (int i = 2; i <= x; i+2)
-            {
-                sum += i;
-                mul *= i;
-            }
-            Console.WriteLine(sum);
-            Console.WriteLine(mul);
-        }
-        static void Odd (int x)
-        {
-            int sum = 0;
-            int mul = 1;
-            for (int i = 1; i <= x; i+2)
-            {
-                sum += i;
-                mul *= i;
-            }
-            Console.WriteLine(sum);
-            Console.WriteLine(mul);
+            double res = (0.35 * m) + (0.65 * p);
+            return res;
         }
     }
 }
