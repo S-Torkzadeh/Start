9bc8e57f9387c10e3712f9b83c5338ce625899ed
S-Torkzadeh <147639939+S-Torkzadeh@users.noreply.github.com>
2023-11-22 20:37:03 +0330

    Add files via upload

---
+++ Kmm&Bmm.cs
@@ -0,0 +1,33 @@
+﻿//محاسبه "ک م م" و "ب م م" اعداد
+using System;
+namespace Project
+{
+    class test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Number 1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kmm is:" + Kmm(num1, num2));
+            Console.WriteLine("Bmm is:" + Bmm(num1, num2));
+        }
+        static int Kmm(int x, int y)
+        {
+            int temp = x * y / Bmm(x, y);
+            return temp;
+        }
+        static int Bmm(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+    }
+}
