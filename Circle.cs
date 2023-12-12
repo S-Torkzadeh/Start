93e32e318704ef44ba0b0496f7bfc4aff6d087f2
S-Torkzadeh <147639939+S-Torkzadeh@users.noreply.github.com>
2023-12-12 20:58:55 +0330

    start

---
+++ Circle.cs
@@ -0,0 +1,47 @@
+﻿// محاسبه محیط و مساحت دایره با استفاده از شی گرایی
+
+using System;
+
+namespace Project
+{
+    public class Circle
+    {
+        private double r;
+        double pi = 3.14;
+        public double R
+        {
+            get { return r;}
+            set
+            {
+                if (value < 0)
+                { r = value;}
+            }
+        }
+        public Circle() { }
+        public Circle(double R)
+        {
+            r = R;
+        }
+        public double S()
+        {
+            double res = pi + Math.Pow(r, 2);
+            return res;
+        }
+        public double A()
+        {
+            double res = pi*r*2;
+            return res;
+        }
+    }
+    class test
+    {
+        public static void Main()
+        {
+            Circle C = new Circle(12);
+            double a = C.A();
+            double s = C.S();
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Peyment.cs
@@ -0,0 +1,66 @@
+﻿// دریافت اطلاعات و چاپ مباغ حق التدریس
+
+using System;
+
+namespace Project
+{
+    public class Teacher
+    {
+        private string id;
+        private string name;
+        private string lastname;
+        private int hour;
+        private int peyperonehour;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public int Hour
+        {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Peyperonehour
+        {
+            get { return peyperonehour; }
+            set
+            {
+                if (value < 0)
+                { peyperonehour = value; }
+            }
+        }
+        public Teacher() { }
+        public Teacher(string Id, string Name, string Lastname, int Hour, int Peyperonehour)
+        {
+            id = Id;
+            name = Name;
+            lastname = Lastname;
+            hour = Hour;
+            peyperonehour = Peyperonehour;
+        }
+        public double Peyment()
+        {
+            return hour * peyperonehour;
+        }
+    }
+    class test
+    {
+        public static void Main()
+        {
+            Teacher th = new Teacher("123", "Ab", "Ahmad", 40, 40000);
+            double Pey = th.Peyment();
+            Console.WriteLine(Pey);
+        }
+    }
+}
